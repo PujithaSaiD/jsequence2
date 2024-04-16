c419f1fdb5d5ca67bbca01f8afa4af2b945c9c43
PujithaSaiD <147297735+PujithaSaiD@users.noreply.github.com>
2024-04-16 13:25:01 +0530

    Add files via upload

---
+++ Sequence2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sequence2 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        List<String> s=new ArrayList<String>();
+        for(int i=0;i<=n;i++){
+            System.out.print(i);
+        }
+        for(int j=n-1;j>=0;j--){
+            System.out.print(j);
+            }
+        
+    }
+}
